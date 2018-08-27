ba2556519ec2f9cdfbdf53eeb02522ae50183958
Alberto Rocha <arocha@ptinovacao.pt>
2018-08-27 08:57:14 +0000

    a test to check the port availability on a VNF

--- tests/ports-test/Tests/runner.sh
+++ tests/ports-test/Tests/runner.sh
@@ -1,2 +1,2 @@
-docker pull sonatanfv/tng-probe-ping
-docker run --rm -v tee:/workspace sonatanfv/tng-probe-ping ${workspace.absolutePath}/config.cfg
+docker pull sonatanfv/tng-probe-ports
+docker run --rm -v tee:/workspace sonatanfv/tng-probe-ports ${workspace.absolutePath}/config.cfg
