3763d9f4150b3d898d067d2d46d09bbd51393003
Felipe Vicens <felipe.vicens@atos.net>
2018-08-30 20:13:28 +0200

    Updating tests

--- tests/probes/wrk2/entrypoint.sh
+++ tests/probes/wrk2/entrypoint.sh
@@ -63,7 +63,7 @@ else
   opt7="--header $HEADER"
 fi
 
-if [ $PROXY = "yes" ]; then
+if [ "$PROXY" = "yes" ]; then
   config="/app/result_proxy.lua"
 else
   config="/app/result.lua"
@@ -73,83 +73,55 @@ fi
 echo "COMMAND: /usr/local/bin/wrk -s $config $opt2 $opt3 $opt4 $opt5 --rate $RATE $opt7 --latency $opt1"
 /usr/local/bin/wrk -s $config $opt2 $opt3 $opt4 $opt5 --rate $RATE $opt7 --latency $opt1 > $RATE.tmp
 cat $RATE.tmp >>  $LogFile
-/bin/cat $RATE.tmp | tail -90 | jq '{ requests, duration_in_microseconds, bytes, requests_per_sec, bytes_transfer_per_sec, latency_distribution }' > rate-$RATE.json
-/bin/cat $RATE.tmp | tail -90 | jq '{ graphs }' > graphs-$RATE.json
-/bin/cat $RATE.tmp | tail -90 | jq "{ requests_per_sec, \"requests\": $RATE }" > overall-$RATE.json
+/bin/cat $RATE.tmp | tail -58 | jq '{ requests, duration_in_microseconds, bytes, requests_per_sec, bytes_transfer_per_sec, latency_distribution }' > rate-$RATE.json
+/bin/cat $RATE.tmp | tail -58 | jq '{ graphs }' > graphs-$RATE.json
+/bin/cat $RATE.tmp | tail -58 | jq "{ requests_per_sec, \"requests\": $RATE }" > overall-$RATE.json
 
 # Processing data
 
 # Creating details.json file
 
-let counter=0
-
 # Obtain the json generated list
-#requests=`ls rate*.json | tr "." "\n" | grep -v json | tr -d "rate-" | xargs`
-
 jsondetail=`echo '{"details": [] }' | jq .`
-
-for i in $RATES
-do
-  join_json=`cat rate-$i.json`
-  let iteration=$counter+1
-  jsondetail=`echo $jsondetail | jq ".details[$counter] +=  $join_json"`
-  let counter++
-done
-
+join_json=`cat rate-$RATE.json`
+jsondetail=`echo $jsondetail | jq ".details[0] +=  $join_json"`
 jsondetail=`echo $jsondetail | jq [.]`
-
 echo $jsondetail | jq . > details.json
 
 # Creating graphs object
-
-# Variables initialization
-let counter=0
-
-# Obtain the json generated list
-#requests=`ls graphs*.json | tr "." "\n" | grep -v json | tr -d "graphs-" | xargs`
-
 graphs=`echo '{"graphs": [] }' | jq .`
 
 # Adding data to graphs object
-for j in $RATES
-do
-  join_json=`cat graphs-$j.json | jq .graphs`
-  graphs=`echo $graphs | jq ".graphs += $join_json"`
-  let counter++
-done
+join_json=`cat graphs-$RATE.json | jq .graphs`
+graphs=`echo $graphs | jq ".graphs += $join_json"`
 
 echo $graphs | jq .graphs > graphs.json
 graphs=`echo $graphs | jq .graphs`
 
 # Variables initialization
-let counter=0
-
-# Obtain the json generated list
-#requests=`ls overall*.json | tr "." "\n" | sed 's/\overall-//g' | grep -v json | xargs`
-
 # Graph template
-json=`echo '{"graphs": [ { "title": "Http Benchmark test", "x-axis-title": "Iteration", "x-axis-unit": "#", "y-axis-title": "Requests per second", "y-axis-unit": "rps", "type": "line", "series": { "s1": "requests_sent", "s2": "requests_processed" }, "data": { "s1": [], "s2": [] } }]} '`
+json=`echo '{"graphs": [ { "title": "Http Benchmark test", "x-axis-title": "Iteration", "x-axis-unit": "#", "y-axis-title": "Requests per second", "y-axis-unit": "rps", "type": "line", "series": { "s1": "requests_sent", "s2": "requests_processed" }, "data": { "s1x": [], "s1y": [], "s2x": [], "s2y": [] } }]} '`
 
 # Adding data to graphs object
-for i in $RATES
-do
-  join_json=`cat overall-$i.json`
-  let iteration=$counter+1
-  s1=`echo $join_json | jq '.requests_per_sec'`
-  s2=`echo $join_json | jq '.request_sent'`
-  json=`echo $json | jq ".graphs[0].data.s1[$counter] += { \"x-axis\": $iteration, \"y-axis\": $s1 }"`
-  json=`echo $json | jq ".graphs[0].data.s2[$counter] += { \"x-axis\": $iteration, \"y-axis\": $s2 }"`
-  let counter++
-done
+join_json=`cat overall-$RATE.json`
+#echo "join_json $join_json"
+
+s1=`echo $join_json | jq '.requests_per_sec'`
+s2=`echo $join_json | jq '.requests'`
+json=`echo $json | jq ".graphs[0].data.s1x += [1]"`
+json=`echo $json | jq ".graphs[0].data.s1y += [$s1]"`
+json=`echo $json | jq ".graphs[0].data.s2x += [1]"`
+json=`echo $json | jq ".graphs[0].data.s2y += [$s2]"`
 
 echo $json | jq . > requests.json
+#echo $graphs
 
 final_graph_object=`echo $json | jq ".graphs += $graphs"`
 the_graphs=`echo $final_graph_object | jq '.graphs'`
-echo "the_graphs" $the_graphs
+#echo "the_graphs" $the_graphs
 
 detail_json=`echo $jsondetail | jq ". += [$final_graph_object]"`
 the_json=`echo $jsondetail | jq '.[].details'`
 detail_sin=`echo $detail_json | jq "{ details: $the_json, graphs: $the_graphs}"`
 echo  $detail_sin | jq . > $DataFile
-echo "detail_sin" $detail_sin
+#echo "detail_sin" $detail_sin
